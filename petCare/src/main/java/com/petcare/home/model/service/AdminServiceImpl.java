d3bf54e83b8f59d425cb38b806c4c8e6bdd605a5
HyunJun123456 <juni303@chauniv.ac.kr>
2022-07-14 15:35:18 +0900

    admincheck 기능

--- petCare/src/main/java/com/petcare/home/model/service/AdminServiceImpl.java
+++ petCare/src/main/java/com/petcare/home/model/service/AdminServiceImpl.java
@@ -1,9 +1,12 @@
 package com.petcare.home.model.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.petcare.home.model.dto.AdminDto;
+import com.petcare.home.model.dto.HospitalDto;
 import com.petcare.home.model.mapper.AdminMapper;
 
 
@@ -13,10 +16,20 @@ public class AdminServiceImpl implements AdminService{
 	@Autowired
 	AdminMapper adminMapper;
 	
+	
 	@Override
 	public AdminDto AdminChk(String AdminId) {
 		return adminMapper.AdminChk(AdminId);
 	}
+
+
+	@Override
+	public List<HospitalDto> HospitalVChk() {
+		// TODO Auto-generated method stub
+		return adminMapper.HospitalVChk();
+	}
+
+
 
 
 
