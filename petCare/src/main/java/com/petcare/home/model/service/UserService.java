7a57bc218d32bd71c20619e76702b4a2e30f6803
HyunJun123456 <juni303@chauniv.ac.kr>
2022-07-19 17:19:31 +0900

    ocr 변경

--- petCare/src/main/java/com/petcare/home/model/service/UserService.java
+++ petCare/src/main/java/com/petcare/home/model/service/UserService.java
@@ -8,7 +8,7 @@ public interface UserService {
 
 	public UserDto UserChk(String userId);
 	
-	public int  updateUserNick(String userid, String usernick);
+public int  updateUserNick(String userid, String usernick);
 	
 	public int updateUserEmail(String userid, String useremail);
 	
