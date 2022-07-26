c102e8b601bab48c0c46a378145f5d15acee3b00
HyunJun123456 <juni303@chauniv.ac.kr>
2022-07-27 08:52:07 +0900

    펫 if문 수정

--- petCare/src/main/java/com/petcare/home/model/service/PetService.java
+++ petCare/src/main/java/com/petcare/home/model/service/PetService.java
@@ -3,9 +3,12 @@ package com.petcare.home.model.service;
 import java.util.List;
 
 import com.petcare.home.model.dto.PetDto;
+import com.petcare.home.model.dto.PetInsuDto;
 
 public interface PetService {
 	public int insertPet(PetDto petdto);
 
 	public List<PetDto> selectPetAll(int userkey);
+	
+	public int petInsuPrice(float petInsuAge, String petInsuType);
 }
