26dc4b1b05f3ce3200f91e5d92ebfab9728c5d69
HyunJun123456 <juni303@chauniv.ac.kr>
2022-08-16 16:11:31 +0900

    자바스크립트 수정

--- petCare/src/main/java/com/petcare/home/controller/BoardController.java
+++ petCare/src/main/java/com/petcare/home/controller/BoardController.java
@@ -49,7 +49,7 @@ public class BoardController {
 		} catch (Exception e) {
 			session.setAttribute("number", 5);
 			
-			return "redirect:/";
+			return "redirect:/user/login";
 			
 		}
  
--- petCare/src/main/resources/static/js/login.js
+++ petCare/src/main/resources/static/js/login.js
@@ -3,31 +3,13 @@
  */
  
  /* 		html을 순서대로 읽음 */
-	numb  = ${num};	
 	
-	
-	if(numb == 4){
 
-		alert("아이디를 다시 확인해주세요!");
-		$.ajax({
-			type:"get",
-			url:"/user/cleanNumb"
-		});
-	}else if(numb == 5){
-		alert("비밀번호를 다시 확인해주세요!");
-		$.ajax({
-			type:"get",
-			url:"/user/cleanNumb"
-		});
-	}else if(numb == 3){
-		alert("일치하는 회원이 없습니다!");
-		$.ajax({
-			type:"get",
-			url:"/user/cleanNumb"
-		});
-	}else{
-		
-	}
+
+	
+	
+	
+
 
 let y = document.getElementById("submit-btn").onclick = function(){isTest();};
 		
