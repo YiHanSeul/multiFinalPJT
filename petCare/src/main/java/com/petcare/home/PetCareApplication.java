4bec1d6e8ee0f2e4ff03fe59cd2f474481c918c3
yihanseul <ihanseul731@gmail.com>
2022-08-17 09:17:06 +0900

    commit

--- petCare/src/main/java/com/petcare/home/PetCareApplication.java
+++ petCare/src/main/java/com/petcare/home/PetCareApplication.java
@@ -1,16 +1,7 @@
 package com.petcare.home;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
-
-
-
-
 
 @SpringBootApplication
 public class PetCareApplication {
